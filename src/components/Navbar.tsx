827a47f4ff5273088e3dc118b650862d1ec939f7
AlbinWr <albin.wreeby@gmail.com>
2025-10-01 15:28:05 +0200

    Navbar centrerad

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,31 +7,26 @@ export const Navbar = () => {
   const { saldo } = useSaldo();
 
   return (
-    <nav className="no-underline bg-slate-800 shadow-lg flex items-center justify-around py-3 px-32 fixed top-0 left-0 w-full">
+    <nav className="fixed top-0 left-0 w-full bg-slate-800 shadow-lg flex items-center justify-between px-8 py-3">
+
+      {/* Logotyp och titel */}
       <NavLink to="/">
-        <span className="font-semibold text-lg flex items-center gap-3 text-blue-400 fixed top-0 left-0">
-          <BiLogoReact className="text-6xl" />
+        <div className="flex items-center gap-3 text-blue-400">
+          <BiLogoReact className="text-5xl" />
           <span className="font-semibold text-2xl">AktieX</span>
-        </span>
+        </div>
       </NavLink>
 
-      <div className="flex items-center gap-5 text-black">
-        {/* Saldot */}
-        <div className="text-white text-sm">
-          Saldo:{" "}
-          <span className="font-semibold">
-            {saldo.toLocaleString("sv-SE")} SEK
-          </span>
-        </div>
-
-        {/* Navigationslänkar */}
+      {/* Navigationslänkar */}
+      <div className="flex items-center gap-6">
         <NavLink
           to="/"
           className={({ isActive }) =>
             isActive
               ? "relative py-1 px-3 text-lg font-light text-sky-300 rounded-2xl bg-slate-700 transition duration-300 group"
               : "relative py-1 px-3 text-lg font-light text-white  hover:text-sky-300 rounded-2xl hover:bg-slate-700 transition duration-300 group"
-          }>
+          }
+        >
           Home
           <span className="absolute left-0 -bottom-1 h-[2px] w-full origin-center scale-x-0 bg-sky-300 transition-transform duration-300 group-hover:scale-x-100"></span>
         </NavLink>
@@ -42,7 +37,8 @@ export const Navbar = () => {
             isActive
               ? "relative py-1 px-3 text-lg font-light text-sky-300 rounded-2xl bg-slate-700 transition duration-300 group"
               : "relative py-1 px-3 text-lg font-light text-white  hover:text-sky-300 rounded-2xl hover:bg-slate-700 transition duration-300 group"
-          }>
+          }
+        >
           Aktier
           <span className="absolute left-0 -bottom-1 h-[2px] w-full origin-center scale-x-0 bg-sky-300 transition-transform duration-300 group-hover:scale-x-100"></span>
         </NavLink>
@@ -53,7 +49,8 @@ export const Navbar = () => {
             isActive
               ? "relative py-1 px-3 text-lg font-light text-sky-300 rounded-2xl bg-slate-700 transition duration-300 group"
               : "relative py-1 px-3 text-lg font-light text-white  hover:text-sky-300 rounded-2xl hover:bg-slate-700 transition duration-300 group"
-          }>
+          }
+        >
           Inställningar
           <span className="absolute left-0 -bottom-1 h-[2px] w-full origin-center scale-x-0 bg-sky-300 transition-transform duration-300 group-hover:scale-x-100"></span>
         </NavLink>
@@ -64,11 +61,20 @@ export const Navbar = () => {
             isActive
               ? "relative py-1 px-3 text-lg font-light text-sky-300 rounded-2xl bg-slate-700 transition duration-300 group"
               : "relative py-1 px-3 text-lg font-light text-white  hover:text-sky-300 rounded-2xl hover:bg-slate-700 transition duration-300 group"
-          }>
+          }
+        >
           Contact
           <span className="absolute left-0 -bottom-1 h-[2px] w-full origin-center scale-x-0 bg-sky-300 transition-transform duration-300 group-hover:scale-x-100"></span>
         </NavLink>
       </div>
+      
+        {/* Saldot */}
+        <div className="bg-slate-700 text-white text-sm px-3 py-1 rounded-full shadow">
+          Saldo:{" "}
+          <span className="font-semibold text-orange-300">
+            {saldo.toLocaleString("sv-SE")} SEK
+          </span>
+        </div>
     </nav>
   );
 };
